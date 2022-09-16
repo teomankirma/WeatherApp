ae0858dd06700d45beb21eb5b5e2f4be3c617194
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-09-16 14:33:30 +0300

    feat: added current day object

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const getWeatherInfo = async function () {
-  const res = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=izmir&appid=853349c5d8cf93911a4f62ac59503dd4&units=metric"
-  );
+let today = new Date();
+let todayDate = today.getDate().toString();
+let todayMonth = today.toLocaleString("default", { month: "long" });
+let todayYear = today.getFullYear().toString();
 
-  const data = await res.json();
+const html = document.getElementById("header");
+html.insertAdjacentHTML(
+  "afterend",
+  `<p>As of ${todayMonth} ${todayDate}, ${todayYear}</p>`
+);
 
-  return console.log(data);
-};
+// const getWeatherInfo = async function () {
+//   const res = await fetch(
+//     "https://api.openweathermap.org/data/2.5/weather?q=izmir&appid=853349c5d8cf93911a4f62ac59503dd4&units=metric"
+//   );
 
-getWeatherInfo();
+//   const data = await res.json();
+
+//   return console.log(data);
+// };
+
+// getWeatherInfo();
